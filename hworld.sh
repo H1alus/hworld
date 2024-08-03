ff7bd2663d4f735127c698a3c71259f32cf87c6b
Vittorio Folino <48564957+H1alus@users.noreply.github.com>
2024-08-03 18:01:30 +0200

    Update hworld.sh

--- hworld.sh
+++ hworld.sh
@@ -2,9 +2,6 @@
 # Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
 # SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
-# Author: Pierre Guillod <pierre.guillod@epfl.ch>, EPFL, STI-SEL
-# Date: 19.02.2022
-
 echo "Generating RTL"
 ./../../vendor/pulp_platform_register_interface/vendor/lowrisc_opentitan/util/regtool.py -r -t rtl data/hworld.hjson
 echo "Generating SW"
